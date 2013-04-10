c4888580c5aeb92eecd87712847a23fde7f8ae69
Echedey Lima <echedeylima@gmail.com>
2013-04-11 00:48:58 +0100

    listar sensores disponibles en el dispositivo

--- src/com/echedeylima/sensores/SensorsActivity.java
+++ src/com/echedeylima/sensores/SensorsActivity.java
@@ -1,15 +1,33 @@
 package com.echedeylima.sensores;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import java.util.List;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class SensorsActivity extends Activity {
+
+	private TextView salida;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		salida = (TextView) findViewById(R.id.salida);
+		SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		List<Sensor> listaSensores = sensorManager
+				.getSensorList(Sensor.TYPE_ALL);
+		for (Sensor sensor : listaSensores)
+			log(sensor.getName());
+
+	}
+
+	private void log(String string) {
+		salida.append(string + "\n");
 	}
 
 	@Override
